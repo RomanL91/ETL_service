30b4a277e1f45ceabb22abf1965cd7efea194a8b
RomanL91 <dev_roman91@mail.ru>
2024-07-29 13:15:22 +0500

    добавление настроек для etl каспи

--- core/settings.py
+++ core/settings.py
@@ -14,8 +14,21 @@ class SettingsETLproc1C(BaseModel):
     api_v1_prefix: str = "/v1/1c/etl"
 
 
+class SettingsETLprocKaspi(BaseModel):
+    url_api_get_customers_id: str = "http://127.0.0.1:8000/api/v1/customers/"
+    url_api_get_products_kaspi_id: str = "http://127.0.0.1:8000/api/v1/kaspi_products/"
+    url_api_get_orders_id: str = "http://127.0.0.1:8000/api/v1/orders/"
+    headers: dict = {
+        "Content-Type": "application/vnd.api+json",
+        "X-Auth-Token": None,
+        "User-Agent": "MyApp/1.0 (http://example.com)",
+    }
+    api_v1_prefix: str = "/v1/kaspi/etl"
+
+
 class Settings(BaseSettings):
     elt_1c: SettingsETLproc1C = SettingsETLproc1C()
+    etl_kaspi: SettingsETLprocKaspi = SettingsETLprocKaspi()
 
 
 settings = Settings()
