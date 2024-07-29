bb645e18444f7229fad6742305dcf3531372fa41
RomanL91 <dev_roman91@mail.ru>
2024-07-29 13:20:13 +0500

    регистрация в main нового etl сервиса

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from core.settings import settings
-from etl_1c.views import router
+from etl_1c.views import router as router_1c
+from etl_kaspi.views import router as router_kaspi
 
 app = FastAPI()
 
-app.include_router(router=router, prefix=settings.elt_1c.api_v1_prefix)
+app.include_router(router=router_1c, prefix=settings.elt_1c.api_v1_prefix)
+app.include_router(router=router_kaspi, prefix=settings.etl_kaspi.api_v1_prefix)
 
 
 if __name__ == "__main__":
